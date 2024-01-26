3b8cc84606c8f1471e45977143c7067540d6b02b
Sedjro2002 <habibhbn3@gmail.com>
2024-01-26 09:06:03 +0100

    refonte

--- locaplus_api/locaplus_api/urls.py
+++ locaplus_api/locaplus_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rental_project project.
+URL configuration for locaplus_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- locaplus_api/users/apps.py
+++ locaplus_api/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RentalAppConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rental_app'
+    name = 'users'
