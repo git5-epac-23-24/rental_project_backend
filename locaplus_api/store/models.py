4f6fcd037c0db14d5e34cb112b4b2505f1737f0b
Sedjro2002 <habibhbn3@gmail.com>
2024-02-10 23:06:16 +0100

    .

---
+++ locaplus_api/store/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from users.models import Customer, Owner
+
+
+
+
+# Create your models here.
+
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "product_types"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=50)
+    location = models.CharField()
+    availability = models.BooleanField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    type = models.ForeignKey(
+        ProductType, on_delete=models.CASCADE, related_name="products"
+    )
+    picture = models.ImageField(upload_to="products/pictures/%Y/%m/%d/", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "products"
+
+    def __str__(self):
+        return self.name
+
+
+
+class Rent(models.model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="rents"
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="rents")
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    duration = models.DurationField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "rents"
+
+    def __str__(self):
+        return self.customer.user.username
+
+
+
+
+
