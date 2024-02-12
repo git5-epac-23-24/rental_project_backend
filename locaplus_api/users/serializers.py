10be9d4bc0755d13264794c07ec53782fd7b4ae3
Sedjro2002 <habibhbn3@gmail.com>
2024-02-12 05:22:46 +0100

    users

--- locaplus_api/users/serializers.py
+++ locaplus_api/users/serializers.py
@@ -1,15 +1,102 @@
 from django.contrib.auth.models import Group
-from users.models import User
+from users.models import User, Customer, Owner
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url','id', 'username', 'first_name', 'last_name', 'email','phone', 'city','country' , 'groups']
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "city",
+            "country",
+            "groups",
+        ]
+        
+class UserCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "city",
+            "country",
+            "password",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.email = validated_data.get("email", instance.email)
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.city = validated_data.get("city", instance.city)
+        instance.country = validated_data.get("country", instance.country)
+        instance.set_password(validated_data.get("password", instance.password))
+        instance.save()
+        return instance
+    
+    
+    
+    
+class UserCreationTestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+        ]
+        # extra_kwargs = {
+        #     "password": {"write_only": True},
+        # }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        instance.email = validated_data.get("email", instance.email)
+        instance.set_password(validated_data.get("password", instance.password))
+        instance.save()
+        return instance    
 
 
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Customer
+        fields = ["id", "user", "address", "created_at", "updated_at"]
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Owner
+        fields = ["id", "user", "address", "created_at", "updated_at"]
+
+
+# class GroupSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Group
-#         fields = ['url', 'name']
+#         fields = [ 'name']
--- locaplus_api/users/views.py
+++ locaplus_api/users/views.py
@@ -1,33 +1,77 @@
 from django.shortcuts import render
 
 from django.contrib.auth.models import Group
-from users.models import User, Customer
+from users.models import User, Customer, Owner
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, CustomerSerializer, OwnerSerializer, UserCreationTestSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from users.models import User, Customer
+from users.serializers import CustomerSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-    
-    
-@api_view(['POST'])
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows customers to be viewed or edited.
+    """
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows owners to be viewed or edited.
+    """
+
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+@api_view(["POST"])
 def register_customer(request):
     data = request.data
-    try:
-        user = User.objects.create_user(
-            data['username'], data['email'], data['password'])
-        # customer = Customer.objects.create(
-        #     user=user)
-        return Response({'status': 'success', 'message': 'User created successfully'})
-    except:
-        return Response({'status': 'error', 'message': 'Something went wrong'})
-    return Response({'status': 'error', 'message': 'User already exists'})
-    return Response(data)
+    serializer = UserCreationTestSerializer(data=data)
+    if serializer.is_valid():
+        user = serializer.save
+        customer = Customer.objects.create(user=user)
+        return Response({
+            "status": "success",
+            "message": "Customer created successfully"
+        })
+    else:
+        return Response({
+            "status": "error",
+            "message": "Something went wrong"
+        })
+    # try:
+    #     user = User.objects.create_user(
+    #         username=data["username"],
+    #         email=data["email"],
+    #         password=data["password"]
+    #     )
+    #     customer = Customer.objects.create(user=user)
+    #     return Response({
+    #         "status": "success",
+    #         "message": "Customer created successfully"
+    #     })
+    # except:
+    #     return Response({
+    #         "status": "error",
+    #         "message": "Something went wrong"
+    #     })
