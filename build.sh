ebef095974e0d77fb5696c75e2cd54b965d3e2d5
marcostar <marcolinmontcho@gmail.com>
2024-02-17 02:39:29 +0100

    build

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-cd locaplus_api/
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
