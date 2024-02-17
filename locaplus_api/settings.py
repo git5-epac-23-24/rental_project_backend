323909089ce7c7a52e73517487f39dc0585d188d
marcostar <marcolinmontcho@gmail.com>
2024-02-17 12:08:15 +0100

    setting

--- locaplus_api/settings.py
+++ locaplus_api/settings.py
@@ -138,6 +138,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/rental_db',
+        # default='postgres://rental_db:veSCORDpybgVjO5Y1dtcAzGHq1WKpJFU@dpg-cn7u8uf79t8c73e7gvh0-a.oregon-postgres.render.com/rental_db',
         conn_max_age=600
     )
     # 'default': {
