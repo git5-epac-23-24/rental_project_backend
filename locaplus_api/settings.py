8dd141e39a366e0e3bdc1050b38027d95108d2fe
Sedjro2002 <habibhbn3@gmail.com>
2024-02-19 12:33:11 +0100

    ...

--- locaplus_api/settings.py
+++ locaplus_api/settings.py
@@ -43,8 +43,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "marcolinsensor@gmail.com"
-EMAIL_HOST_PASSWORD = "xcotzmqvmcjqbufs"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 # EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
 # EMAIL_HOST_USER = 'e72c9e042f0d1f'
